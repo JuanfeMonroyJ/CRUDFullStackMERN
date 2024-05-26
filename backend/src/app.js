55324a6bb8b2c7f10f0c68e8b88b2aa04673da1a
JuanfeMonroyJ <felipemonroyjimenez@gmail.com>
2024-05-26 11:53:42 -0500

    Terminando configuracion localhost 4000

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,4 +5,14 @@ const app = express();
 // Configuración
 app.set('port', process.env.PORT || 4000)
 
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Rutas
+
+app.get('/', (req, res)=>{
+    res.send('Bienvenido a mi api rest full');
+})
+
 module.exports = app;
