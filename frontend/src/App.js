e26b428911194e66a80cb85e303656661a7c8e0d
JuanfeMonroyJ <felipemonroyjimenez@gmail.com>
2024-06-19 07:10:29 -0500

    DOM 3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+import Navegacion from './components/Navegacion'
+import CrearUsuarios from "./components/CrearUsuarios"
+import ListaUsuario from "./components/ListaUsuario"
+
 
 function App() {
   return (
     <div className="">
-      <h2 className="text-center">hola mundo</h2>
+      <Navegacion />
+      <div className="container p-4">
+        <Routes>
+          <Route path="/" element={<ListaUsuarios />} />
+          <Route path="/CrearUsuario" element={<CrearUsuarios />} />
+          <Route path="/edit/:id" element={<CrearUsuarios />} />
+        </Routes>
+
+      </div>
     </div>
   );
 }
