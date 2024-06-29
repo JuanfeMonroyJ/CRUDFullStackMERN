64f2f6399749565b4046887bf536146f1b42776b
JuanfeMonroyJ <felipemonroyjimenez@gmail.com>
2024-06-29 18:21:34 -0500

    Petición Get Frontend

--- frontend/src/components/ListaUsuario.js
+++ frontend/src/components/ListaUsuario.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ListaUsuario = () => {
+
+    const [lista, setLista] = useState([])
+
+    useEffect(() =>{
+            const getUsuarios = async () => {
+                const res = await axios.get("http://localhost:4000/api/usuarios")
+                setLista(res.data)
+            }
+            getUsuarios();
+    },[])
+
     return (
-        <div>ListaUsuario</div>
+        <div className='row'>
+            {
+                lista.map(list => (
+                    <div className='col-md-4 p-2' key={list._id}>
+                        <div className='card'>
+                            <div className='card-header'>
+                                <h5>Nombre: {list.nombre}</h5>
+                            </div>
+                            <div className='card-body'>
+                                <p>Apellido: {list.apellido}</p>
+                                <p>Edad: {list.edad}</p>
+                                <p>Teléfono: {list.telefono}</p>
+                                <p>Correo: {list.correo}</p>
+                            </div>
+
+                            <div className='card-footer'>
+                                <button className='btn btn-danger'>
+                                    Eliminar
+                                </button>
+
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
     )
 }
 
